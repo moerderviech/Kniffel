34ac10717923e1f6fa52090c4d5c0c2f0f7e3032
moerderviech <quinzilein@gmail.com>
2026-04-27 12:15:11 +0200

    Add files via upload

---
+++ Kniffel/dice.py
@@ -0,0 +1,14 @@
+import pygame, random
+
+class Dice:
+    def __init__(self):
+        self.pos.x = x
+        self.pos.y = y
+        self.size = 
+        self.out = False
+
+    def create_dice():
+        pass
+
+    def rolldice():
+        pass
